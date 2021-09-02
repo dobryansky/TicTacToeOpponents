1420c6d867cd94ebb2e7a1978bdbc94015ee5996
Artem <mikstrip@yandex.ru>
2021-09-02 13:57:11 +0300

    Dark Mode Enable

--- app/src/main/java/com/artem/myapplication/FragmentSettings.kt
+++ app/src/main/java/com/artem/myapplication/FragmentSettings.kt
@@ -1,26 +1,77 @@
 package com.artem.myapplication
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceFragment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.CheckBoxPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceManager
 
 
-class FragmentSettings : Fragment() {
+class FragmentSettings : PreferenceFragmentCompat(),SharedPreferences.OnSharedPreferenceChangeListener {
 
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+    }
+    override fun onResume() {
+        super.onResume()
+        preferenceScreen.sharedPreferences
+            .registerOnSharedPreferenceChangeListener(this)
+    }
+    override fun onPause() {
+        super.onPause()
+        preferenceScreen.sharedPreferences
+            .unregisterOnSharedPreferenceChangeListener(this)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
 
+        val darkModeString = getString(R.string.dark_mode)
+
+        if (key == darkModeString) {
+
+            val pref = sharedPreferences?.getString(key, "1")
+
+            when (pref?.toInt()) {
+                1 -> {
+                    AppCompatDelegate
+                        .setDefaultNightMode(
+                            AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                        )
+                }
+
+                2 -> AppCompatDelegate
+                    .setDefaultNightMode(
+                        AppCompatDelegate.MODE_NIGHT_NO
+                    )
+                3 -> AppCompatDelegate
+                    .setDefaultNightMode(
+                        AppCompatDelegate.MODE_NIGHT_YES
+                    )
+                4 -> AppCompatDelegate
+                    .setDefaultNightMode(
+                        AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                    )
+            }
+        }
     }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        PreferenceManager.getDefaultSharedPreferences(context)
+            .unregisterOnSharedPreferenceChangeListener(this)
+    }
 }
+
+
+
--- app/src/main/java/com/artem/myapplication/MainActivity.kt
+++ app/src/main/java/com/artem/myapplication/MainActivity.kt
@@ -2,23 +2,57 @@ package com.artem.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import androidx.preference.PreferenceManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      val bottomNavigationView =findViewById<BottomNavigationView>(R.id.bottomNavView)
-
-        val  navController=findNavController(R.id.fragmentContainerView)
-
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavView)
+        val navController = findNavController(R.id.fragmentContainerView)
         bottomNavigationView.setupWithNavController(navController)
         /*val appBarConfiguration= AppBarConfiguration(setOf(R.id.fragmentSettings))
         setupActionBarWithNavController(navController,appBarConfiguration)*/
+        loadSettings()
+
+    }
+
+
+
+
+    fun loadSettings() {
+        val sp = PreferenceManager.getDefaultSharedPreferences(this)
+
+        val darkMode = sp.getString("Dark Mode", "1")
+
+        when (darkMode?.toInt()) {
+            1 -> {
+                AppCompatDelegate
+                    .setDefaultNightMode(
+                        AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    )
+            }
+
+            2 -> AppCompatDelegate
+                .setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_NO
+                )
+            3 -> AppCompatDelegate
+                .setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_YES
+                )
+            4 -> AppCompatDelegate
+                .setDefaultNightMode(
+                    AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+                )
+        }
+
     }
 }
